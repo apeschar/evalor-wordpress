513609301e548a40723fc9958cdb4fc2e4fdba6f
Albert Peschar <albert@peschar.net>
2014-03-28 11:04:03 +0000

    rename webwinkelkeur.php to evalor.php

--- evalor/evalor.php
+++ evalor/evalor.php
@@ -1,8 +1,8 @@
 <?php
 /*
 Plugin Name: eValor
-Plugin URI: https://www.webwinkelkeur.nl/webwinkel/mogelijkheden/wordpress-module/
-Description: De WordPress plugin zorgt voor een eenvoudige integratie van het eValor binnen jouw webwinkel. Hiermee is het heel eenvoudig om de innovatieve <a href="https://www.webwinkelkeur.nl/webwinkel/mogelijkheden/sidebar/">eValor Sidebar</a> in jouw WordPress website of WooCommerce webwinkel te integreren. Wanneer je WooCommerce gebruikt, kunnen er bovendien automatisch uitnodigingen naar je klanten worden gestuurd.
+Plugin URI: https://www.evalor.es/tienda/modulo/wordpress/
+Description: El módulo WordPress facilita la integración del innovador sidebar de evalor en su web WordPress o tienda WooCommerce.  Para los usuarios del WooCommerce Plugin para WordPress el módulo se encarga de invitar de forma automática a los clientes para que compartan su experiencia. Esta funcion solo esta disponible para clientes PLUS.  Cuando el proceso del pedido ha finalizado se envia automaticamente una invitacion al cliente para que comparta su experiencia. Asi se genera confianza y aumenta la conversion de su tienda online. 
 Version: 1.1.0
 Author: Albert Peschar
 Author URI: https://peschar.net/
